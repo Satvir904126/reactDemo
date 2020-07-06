a0b5fd074196fedc4704d08e0bb8d1540a91e38c
Satvir Singh Satvir Singh <ssatvirs@isimtl.local>
2020-07-06 19:03:00 -0400

    Updates

---
+++ precache-manifest.8eb6ca080452f325c2ec40e556c01313.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b211c373619560c1edd1811d407f0c9",
+    "url": "/reacttodoDemo/index.html"
+  },
+  {
+    "revision": "c3fab20615be6dfe3a3e",
+    "url": "/reacttodoDemo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f4cd8603cbc98fa8da98",
+    "url": "/reacttodoDemo/static/js/2.730fe4ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttodoDemo/static/js/2.730fe4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3fab20615be6dfe3a3e",
+    "url": "/reacttodoDemo/static/js/main.41e7b6a1.chunk.js"
+  },
+  {
+    "revision": "75f4b8da65de008dc133",
+    "url": "/reacttodoDemo/static/js/runtime-main.6a4f136d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacttodoDemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
