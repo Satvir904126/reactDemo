f8a848a3d000dad6a22c20febf1bd04aa6f21ddf
Satvir Singh Satvir Singh <ssatvirs@isimtl.local>
2020-07-13 08:53:56 -0400

    testing

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Helooooooo i am Satvir how r you mohit i did that watch this biro
+    </div>
+  );
+}
+
+export default App;
